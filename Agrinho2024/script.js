efb021da46024e0348d31b691aab985f1faee5b7
Bartoszyk <130589562+marcosbartozyk@users.noreply.github.com>
2024-08-09 21:00:24 -0300

    Carregando versão do agrinho completa

---
+++ Agrinho2024/script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal('.scrollReveal-card', {
+    duration: 1000,
+    rotate:{x:100, y:0, z:0},
+})
+
+sr.reveal('.scrollReveal-img', {
+    duration: 1800,
+    rotate:{x:0, y:100, z:0},
+})
